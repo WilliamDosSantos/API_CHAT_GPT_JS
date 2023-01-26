54724e3ed9c0712e6396f5bf273791ad5a47849a
Will Dev <williamsantos1506@gmail.com>
2023-01-26 18:08:25 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,55 @@
+const inputQuestion = document.getElementById("inputQuestion");
+const result = document.getElementById("result");
+
+inputQuestion.addEventListener("keypress", (e) => {
+  if (inputQuestion.value && e.key === "Enter") SendQuestion();
+});
+
+const OPENAI_API_KEY = "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";
+
+function SendQuestion() {
+  var sQuestion = inputQuestion.value;
+
+  fetch("https://api.openai.com/v1/completions", {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      Authorization: "Bearer " + OPENAI_API_KEY,
+    },
+    body: JSON.stringify({
+      model: "text-davinci-003",
+      prompt: sQuestion,
+      max_tokens: 2048, // tamanho da resposta
+      temperature: 0.5, // criatividade na resposta
+    }),
+  })
+    .then((response) => response.json())
+    .then((json) => {
+      if (result.value) result.value += "\n";
+
+      if (json.error?.message) {
+        result.value += `Error: ${json.error.message}`;
+      } else if (json.choices?.[0].text) {
+        var text = json.choices[0].text || "Sem resposta";
+
+        result.value += "Chat GPT: " + text;
+      }
+
+      result.scrollTop = result.scrollHeight;
+    })
+    .catch((error) => console.error("Error:", error))
+    .finally(() => {
+      inputQuestion.value = "";
+      inputQuestion.disabled = false;
+      inputQuestion.focus();
+    });
+
+  if (result.value) result.value += "\n\n\n";
+
+  result.value += `Eu: ${sQuestion}`;
+  inputQuestion.value = "Carregando...";
+  inputQuestion.disabled = true;
+
+  result.scrollTop = result.scrollHeight;
+}
